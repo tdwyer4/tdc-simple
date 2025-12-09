56818d26c9746668d2caacf0baa387d90048f2df
Tim Dwyer <timdwyer@Tims-Laptop.local>
2025-12-08 22:48:02 -0600

    deletion of lucid-react to fix error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import MasonryGrid from "@/components/MasonryGrid";
-import { Section } from "lucide-react";
 import SectionTitle from "@/components/SectionTitle";
 import Showcase from "./showcase/page";
 
@@ -9,9 +8,12 @@ export default function Home() {
   return (
     <main>
       <section className={styles.hero}>
-        <h1 className={styles.title}>Visual Design Crafted in Tulsa, Oklahoma</h1>
+        <h1 className={styles.title}>
+          Visual Design Crafted in Tulsa, Oklahoma
+        </h1>
         <p className={styles.subtitle}>
-          Motion Graphics / Graphic Design / Video Editing / UI-UX / Creative Direction
+          Motion Graphics / Graphic Design / Video Editing / UI-UX / Creative
+          Direction
         </p>
       </section>
       <SectionTitle title="Selects" />
